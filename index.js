50089bfb624a31961d7b0f28b9646588a750a4ec
Joe Gesualdo <joegesualdo@gmail.com>
2015-08-23 15:16:26 -0400

    Get sentences using async module

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var request = require("request");
 var cheerio = require("cheerio");
+var async = require("async");
 
 function url(word){
   return "http://app.vocabulary.com/app/1.0/dictionary/search?word="+word+"&tz=America%2FNew_York&tzo=-300&appver=1.0.0" 
 }
 function sentenceUrl(word){
   return "http://corpus.vocabulary.com/api/1.0/examples.json?query="+word+"&maxResults=24&startOffset=0&filter=3&tz=America%2FNew_York&tzo=-300&appver=1.0.0"
+}
+function imageUrls(word){
+  var urls = [];
+  urls.push("http://wordinfo.info/words/images/"+word+".jpg");
+  for(var i = 1; i <= 10; i++){
+    urls.push("http://wordinfo.info/words/images/"+word+"-"+i+".jpg");
+  }
+  return urls;
 }
 
 function getShortDescription(word, callback){
@@ -73,12 +82,40 @@ function getSentences(options, callback){
     }
   }.bind(this, options));
 }
+function getImages(options, callback){
+  var options  = options || {};
+  var word = options["word"]
+
+  if(typeof word == undefined){
+    throw(new Error("Please provide a word"));
+  }
+  var potentialImageUrls = imageUrls(word);
+  var successfulImageUrls = [];
+  async.each(potentialImageUrls, function(url, callback){
+    var potentialImageUrl = url;
+    request(potentialImageUrl, function (options, potentialImageUrl, successfulImageUrls, error, response, body) {
+      if (!error && response.statusCode == 200) {
+        successfulImageUrls.push(potentialImageUrl);
+        callback();
+      } else {
+        callback();
+      }
+    }.bind(this, options, potentialImageUrl, successfulImageUrls));
+  }, function(err){
+    if(!err){
+      callback(null, successfulImageUrls)
+    } else {
+      callback(err, null)
+    }
+  })
+}
 
 var vocabFetcher = {
   getShortDescription: getShortDescription,
   getLongDef: getLongDescription,
   getDefs: getDefs,
-  getSentences: getSentences
+  getSentences: getSentences,
+  getImages: getImages
 }
 
 module.exports = vocabFetcher
